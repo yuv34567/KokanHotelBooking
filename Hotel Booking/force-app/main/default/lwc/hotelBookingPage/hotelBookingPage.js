80f2bc634be29139cf8106002ba1fd7e740349b7
Yuvraj Kuwar <yuvrajkuwar15@gmail.com>
2025-12-23 16:54:03 +0530

    Add Apex controller and wire LWC to save hotel bookings
    - Create insertbookingdata Apex class to insert Booked_Hotel_Info__c
    - Expose method via @AuraEnabled for LWC consumption
    - Update hotelBookingPage.js to collect form data and call Apex
    - Enables submitting booking details from UI to Salesforce

--- Hotel Booking/force-app/main/default/lwc/hotelBookingPage/hotelBookingPage.js
+++ Hotel Booking/force-app/main/default/lwc/hotelBookingPage/hotelBookingPage.js
@@ -1,3 +1,18 @@
 import { LightningElement } from 'lwc';
+import insertbookingdatainobject from '@salesforce/apex/insertbookingdata.insertbookingdatainobject';
+export default class HotelBookingPage extends LightningElement {
 
-export default class HotelBookingPage extends LightningElement {}
+    bookingData = {};
+    handleChange(event){
+        this.bookingData[event.target.name] = event.target.value;
+    }
+    handleSubmit(){
+        insertbookingdatainobject({bookingDatas:this.bookingData})
+        .then(result => {
+            console.log(result);
+        })
+        .catch(error => {
+            console.log(error);
+        })
+    }
+}
